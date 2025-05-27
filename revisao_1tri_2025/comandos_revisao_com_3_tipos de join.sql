713366ab4855396318bb24cb24ac56815f33ce8c
Iago Nunes <155097663+IagoDSN@users.noreply.github.com>
2025-05-27 16:42:18 -0300

    27_05

---
+++ revisao_1tri_2025/comandos_revisao_com_3_tipos de join.sql
@@ -0,0 +1,12 @@
+BEGIN;
+	INSERT INTO alunos VALUES(NULL, "Karla Silva");
+	INSERT INTO alunos VALUES(NULL, "André Telles");
+	INSERT INTO alunos VALUES(NULL, "Roseli Pereira");
+	INSERT INTO alunos VALUES(NULL, "Márcio Teixeira");
+COMMIT;
+
+SELECT * FROM alunos;
+
+SELECT a.nome, n.nota
+FROM alunos AS a LEFT JOIN notas AS n
+ON a.id = n.alunos_id;
